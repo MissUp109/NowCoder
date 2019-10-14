e0684a36b9b58d89b6558511bf738fc4810b28ff
MilkCan <leahlynn109@icloud.com>
2019-10-15 02:09:19 +0800

    reorder_list

--- LeetCode经典/reorder_list.cpp
+++ LeetCode经典/reorder_list.cpp
@@ -19,8 +19,78 @@ struct ListNode {
 
 class Solution {
 public:
+    void reverseList(ListNode* &head){//注意'&'
+        if(!head || !head->next) return;
+        ListNode *cur = head->next;
+        head->next = NULL;
+        ListNode *tmp = NULL;
+        while(cur){
+            tmp = cur->next;
+            cur->next = head;
+            head = cur;
+            cur = tmp;        
+        }
+    }
+
+    void reverse(ListNode * &head){//新建头节点方法
+        ListNode *a = new ListNode(0);
+        ListNode *p = head;
+        ListNode *temp = NULL;
+        while(p){
+            cout << p->val << " ";
+            temp = p->next;
+            p->next = a->next;
+            a->next = p;
+            p = temp;
+        }
+        cout << endl;
+        head = a->next;
+    }
+
     void reorderList(ListNode *head) {
-        
+        if(!head || !head->next || !head->next->next){
+        	return;
+        }
+
+        ListNode *slow = head, *fast = head;
+        while(fast->next && fast->next->next){
+        	slow = slow->next;
+        	fast = fast->next->next;
+        }
+
+        cout << "mid: " << slow->next->val << endl;
+
+        ListNode *p = head;
+        ListNode *q = slow->next;
+        reverseList(q);
+
+        ListNode *tmp = NULL;
+        bool flag = true;
+        while(p && q){
+        	cout << "p: " << p->val << ", q: " << q->val << ", flag: " << flag << endl;
+        	if(flag){
+        		tmp = p->next;
+        		p->next = q;
+        		p = tmp;
+            }
+        	else{
+        		tmp = q->next;
+        		q->next = p;
+        		q = tmp;
+        	}
+        	flag = !flag;
+
+        }
+        if(p) p->next = NULL;
+        if(q) q->next = NULL;
+    }
+
+    void print(ListNode *head){
+    	while(head){
+    		cout << head->val << " -> ";
+    		head = head->next;
+    	}
+    	cout << endl;
     }
 };
 
@@ -38,6 +108,8 @@ int main(){
 	n3->next = n4;
 	n4->next = NULL;
 	
+    //sol.reverse(head);
+    //sol.reverseList(head);
 	sol.reorderList(head);
 	sol.print(head);
 }
