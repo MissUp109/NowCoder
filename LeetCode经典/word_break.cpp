117728482d00e44ae1675a2372038c17e702899f
MilkCan <leahlynn109@icloud.com>
2019-10-17 02:02:33 +0800

    add word break

---
+++ LeetCode经典/word_break.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/*
+给定一个字符串s和一组单词dict，判断s是否可以用空格分割成一个单词序列，使得单词序列中所有的单词都是dict中的单词（序列可以包含一个或多个单词）。
+例如:
+给定s=“leetcode”；
+dict=["leet", "code"].
+返回true，因为"leetcode"可以被分割成"leet code".*/
+
+class Solution {
+public:
+    vector<string> wordBreak(string s, unordered_set<string> &dict) {
+        
+    }
+};
---
+++ LeetCode经典/word_break_ii.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+给定一个字符串s和一组单词dict，在s中添加空格将s变成一个句子，使得句子中的每一个单词都是dict中的单词
+返回所有可能的结果
+例如：给定的字符串s ="catsanddog",
+dict =["cat", "cats", "and", "sand", "dog"].
+
+返回的结果为["cats and dog", "cat sand dog"].
+*/
+
+class Solution {
+public:
+    vector<string> wordBreak(string s, unordered_set<string> &dict) {
+        
+    }
+};
